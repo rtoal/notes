0c4214fb6c7048b138bcb300953850308f0f4c68
Ray Toal <rtoal@lmu.edu>
2017-08-14 02:03:44 -0700

    Configure login in app.yaml

--- main.py
+++ main.py
@@ -7,29 +7,22 @@ env = jinja2.Environment(loader=jinja2.FileSystemLoader('templates'))
 
 class MainHandler(webapp2.RequestHandler):
     def get(self):
+        notes = Note.query().order(-Note.timestamp).fetch(limit=20)
         template = env.get_template('main.html')
-        self.response.write(template.render())
+        self.response.write(template.render({ 'notes': notes }))
 
 class NoteHandler(webapp2.RequestHandler):
     def get(self):
         user = users.get_current_user()
-        if user:
-            template = env.get_template('note_entry.html')
-            self.response.write(template.render({
-                'nickname' : user.nickname(),
-                'logout_url': users.create_logout_url('/')}))
-        else:
-            self.response.write('Please <a href="%s">log in with Google first</a>' %
-                users.create_login_url('/notes'))
+        template = env.get_template('note_entry.html')
+        self.response.write(template.render({
+            'nickname' : user.nickname(),
+            'logout_url': users.create_logout_url('/')}))
     def post(self):
         content = self.request.get('note')
         user = users.get_current_user()
-        if user:
-            note = Note(user_id=user.user_id(), content=content).put()
-            self.response.write('Thank you')
-        else:
-            self.response.write('Please <a href="%s">log in with Google first</a>' %
-                users.create_login_url('/notes'))
+        note = Note(user_id=user.user_id(), content=content).put()
+        self.response.write('Thank you')
 
 app = webapp2.WSGIApplication([
     ('/', MainHandler),
