90a5493e4953da512c51debac0ff159f266976fe
Ray Toal <rtoal@lmu.edu>
2017-08-14 02:09:37 -0700

    Add logout url to main page

--- main.py
+++ main.py
@@ -8,8 +8,10 @@ env = jinja2.Environment(loader=jinja2.FileSystemLoader('templates'))
 class MainHandler(webapp2.RequestHandler):
     def get(self):
         notes = Note.query().order(-Note.timestamp).fetch(limit=20)
+        user = users.get_current_user()
+        logout_url = users.create_logout_url('/') if user else None
         template = env.get_template('main.html')
-        self.response.write(template.render({ 'notes': notes }))
+        self.response.write(template.render({ 'notes': notes, 'logout_url': logout_url }))
 
 class NoteHandler(webapp2.RequestHandler):
     def get(self):
@@ -22,7 +24,7 @@ class NoteHandler(webapp2.RequestHandler):
         content = self.request.get('note')
         user = users.get_current_user()
         note = Note(user_id=user.user_id(), content=content).put()
-        self.response.write('Thank you')
+        self.redirect('/')
 
 app = webapp2.WSGIApplication([
     ('/', MainHandler),
